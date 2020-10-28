5a87f852149367afac485a989d7ad3116ef5c795
candra kriswinarto <ckriswinarto@gmail.com>
2020-10-29 01:58:23 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import PasswordStrengthMeter from './components/PasswordStrengthMeter';
 
 function App() {
+  const [ password, setPassword ] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="col-md-6 mx-auto text-right">
+        <h3 className="text-center my-5">Cand Dev</h3>
+        <div className="form-group mb-1">
+          <input
+            type="password"
+            className="form-control shadow-none"
+            placeholder="Password"
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <PasswordStrengthMeter password={password} />
+      </div>
     </div>
   );
 }
---
+++ src/components/PasswordStrengthMeter.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import zxcvbn from 'zxcvbn';
+
+const PasswordStrengthMeter = ({ password }) => {
+  const testResult = zxcvbn(password);
+  const num = testResult.score * 100/4;
+
+  const createPassLabel = () => {
+    switch(testResult.score) {
+      case 0:
+        return 'Very weak';
+      case 1:
+        return 'Weak';
+      case 2:
+        return 'Fear';
+      case 3:
+        return 'Good';
+      case 4:
+        return 'Strong';
+      default:
+        return '';
+    }
+  }
+
+  const funcProgressColor = () => {
+    switch(testResult.score) {
+      case 0:
+        return '#828282';
+      case 1:
+        return '#EA1111';
+      case 2:
+        return '#FFAD00';
+      case 3:
+        return '#9bc158';
+      case 4:
+        return '#00b500';
+      default:
+        return 'none';
+    }
+  }
+
+  const changePasswordColor = () => ({
+    width: `${num}%`,
+    background: funcProgressColor(),
+    height: '7px'
+  })
+
+  return (
+    <>
+      <div className="progress" style={{ height: '7px' }}>
+        <div className="progress-bar" style={changePasswordColor()}></div>
+      </div>
+      <p style={{ color: funcProgressColor() }}>{createPassLabel()}</p>
+    </>
+  )
+}
+
+export default PasswordStrengthMeter
